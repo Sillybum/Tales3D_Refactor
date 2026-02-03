06af983f87a800ed5c478fe2c3cb7f7b44284015
Sam <woki23601@gmail.com>
2026-02-03 17:45:55 +0900

    탑다운 이동(InputMappingContext)
    - 탑다운 이동 입력 언리얼 권장 IMC로 구현

--- Source/Tales3D_Refactor/Private/Framework/CorePlayerController.cpp
+++ Source/Tales3D_Refactor/Private/Framework/CorePlayerController.cpp
@@ -3,3 +3,93 @@
 
 #include "Framework/CorePlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+ACorePlayerController::ACorePlayerController()
+{
+	// For Left Click Hold
+	PrimaryActorTick.bCanEverTick = true;
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
+
+void ACorePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Registers IMC to Local Player
+	if (ULocalPlayer* LP = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (IMC_Core)
+			{
+				Subsystem->AddMappingContext(IMC_Core, 0);
+			}
+		}
+	}
+}
+
+void ACorePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	// Binds Actions
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		if (IA_MoveHold)
+		{
+			// On LB pushed
+			EIC->BindAction(IA_MoveHold, ETriggerEvent::Started, this, &ACorePlayerController::OnMoveHoldStarted);
+			// On LB being holded
+			EIC->BindAction(IA_MoveHold, ETriggerEvent::Triggered, this, &ACorePlayerController::OnMoveHoldTriggered);
+			// On LB released
+			EIC->BindAction(IA_MoveHold, ETriggerEvent::Completed, this, &ACorePlayerController::OnMoveHoldCompleted);
+			// When things such as window losing focus happen
+			EIC->BindAction(IA_MoveHold, ETriggerEvent::Canceled, this, &ACorePlayerController::OnMoveHoldCompleted);
+			
+		}
+	}
+}
+
+void ACorePlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	if (bMoveHeld)
+	{
+		UpdateDestinationAndMove();
+	}
+}
+
+void ACorePlayerController::OnMoveHoldStarted()
+{
+	bMoveHeld = true;
+	UpdateDestinationAndMove();
+}
+
+void ACorePlayerController::OnMoveHoldTriggered()
+{
+	bMoveHeld = true;
+	UpdateDestinationAndMove();
+}
+
+void ACorePlayerController::OnMoveHoldCompleted()
+{
+	bMoveHeld = false;
+}
+
+void ACorePlayerController::UpdateDestinationAndMove()
+{
+	FHitResult Hit;
+	const bool bHit = GetHitResultUnderCursor(ECC_Visibility, false, Hit);
+	if (!bHit || !Hit.bBlockingHit) return;
+	
+	CachedDestination = Hit.ImpactPoint;
+	// NavMesh oriented Move
+	UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, CachedDestination);
+}
--- Source/Tales3D_Refactor/Private/Framework/CorePlayerController.h
+++ Source/Tales3D_Refactor/Private/Framework/CorePlayerController.h
@@ -6,12 +6,38 @@
 #include "GameFramework/PlayerController.h"
 #include "CorePlayerController.generated.h"
 
-/**
- * 
- */
+class UInputAction;
+class UInputMappingContext;
+
 UCLASS()
 class ACorePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	ACorePlayerController();
 	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	// For mousehold update
+	virtual void PlayerTick(float DeltaTime) override;
+	
+private:
+	// Enhanced Input Assets (be assigned on BP)
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputMappingContext> IMC_Core;
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> IA_MoveHold;
+	
+	// States
+	bool bMoveHeld = false;
+	FVector CachedDestination;
+	
+	// Input Callbacks
+	void OnMoveHoldStarted();
+	void OnMoveHoldTriggered();
+	void OnMoveHoldCompleted();
+	
+	// Actual Move Update
+	void UpdateDestinationAndMove();
 };
